bc7784bc272f1ada92fe868937086d404611ce71
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-05-29 00:52:21 +0300

    add main changes but doesnt work repo

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { useSearchParams } from 'react-router-dom';
-import { Button, Field, Span } from './Form.styled';
+import { Button, Field, Span, StyledForm } from './Form.styled';
 const Form = () => {
     const [_, setParams] = useSearchParams();
 
@@ -25,11 +25,12 @@ const Form = () => {
   }, [isSubmitSuccessful, reset]);
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-       <Field {...register('query', { required: true })} />
+       <StyledForm  {...register('query', { required: true })} />
 
       <Button type="submit">Search</Button>
       {errors.query && <Span>This field is required</Span>}
-    </form>
+      </form>
+      </StyledForm> 
   );
 };
 
