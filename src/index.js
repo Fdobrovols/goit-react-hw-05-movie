22ada21cb257f84a2c22ed13275cd23853117e25
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-05-28 23:42:48 +0300

    repo and some changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Global, ThemeProvider } from '@emotion/react';
+export { default as Home } from './Home/Home.jsx';
+export { default as Movies } from './Movies/Movies.jsx';
 import { App } from 'components/App';
 import './index.css';
+import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode> fgr 
+  <React.StrictMode>
     <App />
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <ThemeProvider theme={theme}>
+        <Global styles={GlobalStyles} />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
