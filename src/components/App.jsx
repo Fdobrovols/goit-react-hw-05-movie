6f3ba0cc893b4d0a9b7a06b3323cf93727c17be2
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-05-29 12:39:27 +0300

    changes new after checked

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { fetchTrendingMovies } from 'services/movieAPI/movieAPI';
+
 
 const Layout = lazy(() => import('./Layout/Layout'));
 const Cast = lazy(() => import('./Cast/Cast'));
@@ -9,7 +9,6 @@ const Home = lazy(() => import('../pages/Home/Home'));
 const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
 const Movies = lazy(() => import('../pages/Movies/Movies'));
 
-fetchTrendingMovies();
 export const App = () => {
   return (
     <>
